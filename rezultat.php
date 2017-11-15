1cae96cda5f38ec8aa5649deb0da7e9b482a014a
Andreas Leja <andreas.leja1@gmail.com>
2017-11-15 12:55:38 +0100

    Dodali smo nove datoteke

---
+++ rezultat.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+<?php
+    echo('Pozdrav, '. $_POST['prezime'].' '.$_POST['ime'].' imate ');
+    //print_r($_POST);
+    if($_POST['G']>=18)
+    {
+        echo('<span style="color: green">'.$_POST['G'].'godina'.'</span>');
+        echo(' ZA SEKS SPERMNI ');
+    }
+    else
+    {
+        echo('<span style="color: red">'.$_POST['G'].'godina'.'</span>');
+        echo(' GLUPO NERAZUMNO DIJETE!');
+    }
+?>
+</body>
+
+</html>
---
+++ unos.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Unos</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <form action="rezultat.php" method="post">
+        Ime:<br>
+        <input tpye="text" name= "ime"><br>
+
+        Prezime:<br>
+        <input type ="text" name= "prezime"><br>
+
+        Godine:<br>
+        <input type ="text" name= "G"><br>
+
+        <input type="submit" value="Pošalji!">
+    </form>
+</body>
+
+</html>
